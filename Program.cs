e1a407b43d338806dd1b7e092322e96044780efd
DAVIDGOMEZARAGONSOFTWAREONE <90686905+DAVIDGOMEZARAGONSOFTWAREONE@users.noreply.github.com>
2021-10-07 18:16:21 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,12 +1,27 @@
-﻿using System;
+using System;
 
 namespace Spaceman
 {
-  class Program
+  internal class Program:Game
   {
     static void Main(string[] args)
     {
-
+      Game xogo=new Game();
+      xogo.Greet();
+      xogo.Display();
+      xogo.Ask();
+      while(!xogo.DidWin() || !xogo.DidLose()){
+        xogo.Display();
+        xogo.Ask();
+        if(xogo.DidWin()){
+          Console.WriteLine("Enhorabuena. Has ganado!");
+          break;
+        }
+        if(xogo.DidLose()){
+          Console.WriteLine("Lo siento. Has perdido");
+          break;
+        }
+      }
     }
   }
 }
